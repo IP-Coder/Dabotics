d32c0dfb9b73ac6ec8f17ec29e4d0b93e096a4b2
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-11 00:36:36 +0530

    update link add target

--- index.php
+++ index.php
@@ -207,7 +207,7 @@
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href=""> <i class="ri-linkedin-box-fill" target="_blank"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -225,7 +225,7 @@
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href="https://www.linkedin.com/in/devilcoder/"> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.linkedin.com/in/devilcoder/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -245,7 +245,7 @@
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href="https://www.linkedin.com/in/suraj-kumar-1a525a1a7/"> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.linkedin.com/in/suraj-kumar-1a525a1a7/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -262,7 +262,7 @@
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href="https://www.linkedin.com/in/prakharsarswat/"> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.linkedin.com/in/prakharsarswat/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
