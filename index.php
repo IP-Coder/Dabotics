518641f14c9bda4588364589f5ad0e23f7170711
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-11 00:06:46 +0530

    updating sde profile

--- index.php
+++ index.php
@@ -237,6 +237,7 @@
                             <div class="pic"><img src="assets/img/team/team-3.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Suraj Kumar</h4>
+                                <h5>From Chandigarh University</h5>
                                 <span>SDE, Python Trainer</span>
                                 <p>Addition of small steps let you closer to yous goal</p>
                                 <div class="social">
