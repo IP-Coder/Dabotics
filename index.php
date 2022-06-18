a25f15c4ef5b21ca231ac9f02dd64ce8e4b26d6e
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-18 15:46:17 +0530

    Cheack

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
                 <div class="modal-header " style="flex-direction:column">
                     <h1 class="text-center">Yey!</h1><br>
                     <h5 class="modal-title text-center ">Join Demo Sessions By
-                        <br>DABiOTICS
+                        <br>DABOTICS
                     </h5>
                     <div class="mt-5 text-center"><a class="btn1" href="WebdevSession.php">Web Development</a>
 
@@ -207,7 +207,7 @@
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href=""> <i class="ri-linkedin-box-fill" target="_blank"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -218,13 +218,14 @@
                             <div class="pic"><img src="assets/img/team/team-2.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Pranjal Shrivastava</h4>
+                                <h6>From Invertis University</h6>
                                 <span>Co-Founder</span>
                                 <p>Believe In Working Not In Thinking</p>
                                 <div class="social">
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
-                                    <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.instagram.com/pranjal.9.0.4.4/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/devilcoder/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -237,13 +238,14 @@
                             <div class="pic"><img src="assets/img/team/team-3.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Suraj Kumar</h4>
-                                <span>SDE</span>
-                                <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
+                                <h6>From Chandigarh University</h6>
+                                <span>SDE, Python Trainer</span>
+                                <p>Addition of small steps let you closer to yous goal</p>
                                 <div class="social">
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
-                                    <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.instagram.com/surajsinhar7/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/suraj-kumar-1a525a1a7/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -253,13 +255,14 @@
                             <div class="pic"><img src="assets/img/team/team-4.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Er. Prakhar Sarswat</h4>
+                                <!-- <h6>From Samsung Electronics</h6> -->
                                 <span>CTO</span>
-                                <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
+                                <p>Don't stop Chasing your Dreams, Beacause Dream</p>
                                 <div class="social">
                                     <a href=""><i class="ri-twitter-fill"></i></a>
                                     <a href=""><i class="ri-facebook-fill"></i></a>
                                     <a href=""><i class="ri-instagram-fill"></i></a>
-                                    <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                                    <a href="https://www.linkedin.com/in/prakharsarswat/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
                                 </div>
                             </div>
                         </div>
@@ -285,6 +288,9 @@
 
                     <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
                         <div class="box featured">
+                            <div class="new_log">
+                                <img src="./assets/img/tnew_logo.png" alt="new logo" width="80">
+                            </div>
                             <h3>Python</h3>
                             <h4><sup>₹
                                 </sup>10000<span> </span></h4>
@@ -306,6 +312,9 @@
                     </div>
                     <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
                         <div class="box featured">
+                        <div class="new_log">
+                                <img src="./assets/img/tnew_logo.png" alt="new logo" width="80">
+                            </div>
                             <h3>AI With Python</h3>
                             <h4><sup>₹
                                 </sup>15000/-<span> </span></h4>
@@ -327,6 +336,9 @@
                     </div>
                     <div class="col-lg-4 mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
                         <div class="box featured">
+                        <div class="new_log">
+                                <img src="./assets/img/tnew_logo.png" alt="new logo" width="80">
+                            </div>
                             <h3>Robotics</h3>
                             <h4><sup>₹
                                 </sup>10000/-<span> </span></h4>
