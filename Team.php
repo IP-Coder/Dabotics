b3b161d9a72ea4a3adc854f33152ed27bf5b53d6
Suraj Kumar <surajsinhaking77@gmail.com>
2022-06-20 08:30:53 +0530

    "update team and add member"

--- Team.php
+++ Team.php
@@ -50,76 +50,99 @@
 
             <div class="row">
 
-                <div class="col-lg-6">
-                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="pic"><img src="assets/img/team/team-1.png" class="img-fluid" alt=""></div>
-                        <div class="member-info">
-                            <h4>Sachin Shrivastava</h4>
-                            <span>Founder</span>
-                            <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
-                            <div class="social">
-                                <a href=""><i class="ri-twitter-fill"></i></a>
-                                <a href=""><i class="ri-facebook-fill"></i></a>
-                                <a href=""><i class="ri-instagram-fill"></i></a>
-                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                    <div class="col-lg-6">
+                        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                            <div class="pic"><img src="assets/img/team/team-1.png" class="img-fluid" alt=""></div>
+                            <div class="member-info">
+                                <h4>Sachin Shrivastava</h4>
+                                <span>Founder</span>
+                                <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
+                                <div class="social">
+                                    <a href=""><i class="ri-twitter-fill"></i></a>
+                                    <a href=""><i class="ri-facebook-fill"></i></a>
+                                    <a href=""><i class="ri-instagram-fill"></i></a>
+                                    <a href=""> <i class="ri-linkedin-box-fill" target="_blank"></i> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 mt-4 mt-lg-0">
+                        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
+                            <div class="pic"><img src="assets/img/team/team-2.png" class="img-fluid" alt=""></div>
+                            <div class="member-info">
+                                <h4>Pranjal Shrivastava</h4>
+                                <h6>From Invertis University</h6>
+                                <span>Co-Founder</span>
+                                <p>Believe In Working Not In Thinking</p>
+                                <div class="social">
+                                    <a href=""><i class="ri-twitter-fill"></i></a>
+                                    <a href=""><i class="ri-facebook-fill"></i></a>
+                                    <a href="https://www.instagram.com/pranjal.9.0.4.4/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/devilcoder/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-5">
+
+                    <div class="col-lg-6">
+                        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                            <div class="pic"><img src="assets/img/team/team-3.png" class="img-fluid" alt=""></div>
+                            <div class="member-info">
+                                <h4>Suraj Kumar</h4>
+                                <h6>From Chandigarh University</h6>
+                                <span>SDE, Python Trainer</span>
+                                <p>Addition of small steps let you closer to yous goal</p>
+                                <div class="social">
+                                    <a href=""><i class="ri-twitter-fill"></i></a>
+                                    <a href=""><i class="ri-facebook-fill"></i></a>
+                                    <a href="https://www.instagram.com/surajsinhar7/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/suraj-kumar-1a525a1a7/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                            <div class="pic"><img src="assets/img/team/team-4.png" class="img-fluid" alt=""></div>
+                            <div class="member-info">
+                                <h4>Er. Prakhar Sarswat</h4>
+                                <!-- <h6>From Samsung Electronics</h6> -->
+                                <span>CTO</span>
+                                <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
+                                <div class="social">
+                                    <a href=""><i class="ri-twitter-fill"></i></a>
+                                    <a href=""><i class="ri-facebook-fill"></i></a>
+                                    <a href=""><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/prakharsarswat/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="col-lg-6 mt-4 mt-lg-0">
-                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="200">
-                        <div class="pic"><img src="assets/img/team/team-2.png" class="img-fluid" alt=""></div>
-                        <div class="member-info">
-                            <h4>Pranjal Shrivastava</h4>
-                            <span>Co-Founder</span>
-                            <p>Believe In Working Not In Thinking</p>
-                            <div class="social">
-                                <a href=""><i class="ri-twitter-fill"></i></a>
-                                <a href=""><i class="ri-facebook-fill"></i></a>
-                                <a href=""><i class="ri-instagram-fill"></i></a>
-                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row mt-5">
+                <div class="row mt-5">
 
-                <div class="col-lg-6">
-                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="pic"><img src="assets/img/team/team-3.png" class="img-fluid" alt=""></div>
-                        <div class="member-info">
-                            <h4>Suraj Kumar</h4>
-                            <span>SDE</span>
-                            <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
-                            <div class="social">
-                                <a href=""><i class="ri-twitter-fill"></i></a>
-                                <a href=""><i class="ri-facebook-fill"></i></a>
-                                <a href=""><i class="ri-instagram-fill"></i></a>
-                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+                    <div class="col-lg-6">
+                        <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
+                            <div class="pic"><img src="assets/img/team/team-5.png" class="img-fluid" alt=""></div>
+                            <div class="member-info">
+                                <h4>Krishna Gupta</h4>
+                                <!-- <h6>From Chandigarh University</h6> -->
+                                <span>Managing Director</span>
+                                <p>Addition of small steps let you closer to yous goal</p>
+                                <div class="social">
+                                    <a href=""><i class="ri-twitter-fill"></i></a>
+                                    <a href=""><i class="ri-facebook-fill"></i></a>
+                                    <a href="https://www.instagram.com/surajsinhar7/" target="_blank"><i class="ri-instagram-fill"></i></a>
+                                    <a href="https://www.linkedin.com/in/krishna-gupta-8416a9242/" target="_blank"> <i class="ri-linkedin-box-fill"></i> </a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="pic"><img src="assets/img/team/team-4.png" class="img-fluid" alt=""></div>
-                        <div class="member-info">
-                            <h4>Er. Prakhar Sarswat</h4>
-                            <span>CTO</span>
-                            <p>Don't stop Chasing your Dreams ,Beacause Dream Do Come True</p>
-                            <div class="social">
-                                <a href=""><i class="ri-twitter-fill"></i></a>
-                                <a href=""><i class="ri-facebook-fill"></i></a>
-                                <a href=""><i class="ri-instagram-fill"></i></a>
-                                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
 
         </div>
     </section><!-- End Team Section -->
