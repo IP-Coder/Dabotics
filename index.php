44155b638de65dfc2474de13146714bf662c3d54
Pranjal Shrivastava <dcs18pranjal.s@invertisuniversity.ac.in>
2022-06-22 23:16:10 +0530

    Update index.php
    nothing

--- index.php
+++ index.php
@@ -128,7 +128,6 @@
                             <div class="pic"><img src="assets/img/team/team-2.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Pranjal Shrivastava</h4>
-                                <h6>From Invertis University</h6>
                                 <span>Co-Founder</span>
                                 <p>Believe In Working Not In Thinking</p>
                                 <div class="social">
@@ -148,7 +147,6 @@
                             <div class="pic"><img src="assets/img/team/team-3.png" class="img-fluid" alt=""></div>
                             <div class="member-info">
                                 <h4>Suraj Kumar</h4>
-                                <h6>From Chandigarh University</h6>
                                 <span>SDE, Python Trainer</span>
                                 <p>Addition of small steps let you closer to yous goal</p>
                                 <div class="social">
