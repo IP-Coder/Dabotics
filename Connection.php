d77208c5edf89447653f7c40a0e1b4b7c53e597c
Pranjal Shrivastava <dcs18pranjal.s@invertisuniversity.ac.in>
2022-08-26 22:54:52 +0530

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,7 +1,7 @@
 <?php
 
 $servername = "localhost";
-$username = "u523284279_daboAdmin";
+$username = "u523284279_DaPranjal";
 $paswowrd = "Invertis@dabotics@9044#uiop";
 $database = "u523284279_dabotics";
 $conn = mysqli_connect($servername, $username, $paswowrd, $database);
