329ae26f33bba8e90630883838af4aea1241d892
Pranjal Shrivastava <dcs18pranjal.s@invertisuniversity.ac.in>
2022-08-28 20:45:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
                     <h1>THE FUTURE OF MOBILITY IS HERE</h1>
                     <h2>Discover the Safest self-driving experience with Dabotics India Pvt. Ltd.</h2>
                     <div class="d-flex justify-content-center justify-content-lg-start">
-                        <a href="#about" class="btn-get-started scrollto">Get Started</a>
+                        <a href="Registration.php" class="btn-get-started scrollto">Get Started</a>
                     </div>
                 </div>
                 <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
