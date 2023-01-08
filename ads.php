a39182567608ddf86549f6d3a486c4f684eb4925
IP-Coder <shrivastavaps80@gmail.com>
2023-01-08 22:58:02 +0530

    ads.phpadding

---
+++ ads.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- My Google Ads Here Dont Touch This Code -->
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545298674584153"
+        crossorigin="anonymous"></script>
+    <!-- FirstAd -->
+    <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7545298674584153" data-ad-slot="4270295808"
+        data-ad-format="auto" data-full-width-responsive="true"></ins>
+    <script>
+    (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    <!-- My Google Ads Here Dont Touch This Code -->
+</body>
+
+</html>
