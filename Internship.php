f2a0af5bc1fdd6c7f49c3667268d8ae597038b94
IP-Coder <shrivastavaps80@gmail.com>
2023-01-08 23:32:59 +0530

    Internship page addition
    Internship page addition

---
+++ Internship.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Registration Form</title>
+    <?php
+    include 'Header.php'
+    ?>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545298674584153"
+        crossorigin="anonymous"></script>
+</head>
+<style>
+.btn1 {
+    background: #47b2e4;
+    border: 0;
+    padding: 12px 34px;
+    color: #fff;
+    transition: 0.4s;
+    border-radius: 50px;
+}
+</style>
+
+<body class="d-flex flex-column min-vh-100">
+    <!-- ======= Header ======= -->
+    <?php
+    include 'Navebar.php'
+    ?>
+    <!-- End Header -->
+    <!-- ======= Form Section ======= -->
+    <div class="conatainer text-center">
+        <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSc0ZuTwOdQ4VsfU1H5y6VYgPgbSHnDubf4nLertNUzkxfB2XA/viewform?embedded=true"
+            width="640" height="3138" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+    <!-- ======= Footer ======= -->
+    <?php
+    include 'Footer.php'
+    ?>
+    <!-- End Footer -->
+
+</body>
+
+</html>
