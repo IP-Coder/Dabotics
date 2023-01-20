1f21f174269d7bafe1078e0fe2c5d700a333f243
IP-Coder <shrivastavaps80@gmail.com>
2023-01-20 22:24:09 +0530

    dfdf

---
+++ Pricing.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <title>Services</title>
+    <?php
+    include 'Header.php'
+    ?>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545298674584153"
+        crossorigin="anonymous"></script>
+    <style>
+    body {
+        font-family: 'times new roman';
+        background-color: rgb(55, 81, 126);
+        color: white;
+    }
+
+    .home {
+        display: flex;
+        align-items: center;
+        color: white;
+        height: 31rem;
+
+    }
+
+    .contact {
+        padding: 0;
+        color: rgb(55, 81, 126);
+    }
+
+
+    .wapp {
+
+        background-color: rgb(97, 206, 112);
+        border: 2px solid white;
+        border-radius: 8px;
+    }
+
+    .wapp:hover {
+        background-color: rgb(55, 81, 126);
+
+        transition: 0.5s;
+    }
+
+    a {
+        color: white;
+    }
+
+    .imgs {
+
+        flex-direction: column-reverse;
+        align-items: center;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <?php
+    include 'Navebar.php'
+    ?>
+    <!-- End Header -->
+    <!-- ======= Services Section ======= -->
+    <section id="services mt-5" class="about">
+        <div class="" data-aos="fade-up">
+            <div class="home ">
+                <div class="container">
+                    <div class="row " style="align-items: center">
+                        <div class="col">
+                            <p class="fs-1">PROFESSIONAL ECOMMERCE WEBSITE DEVELOPMENT COMPANY
+                            </p>
+                            <div class="wapp text-center">
+                                <a target="0"
+                                    href="https://api.whatsapp.com/send/?phone=+918126664363&text=Hi!%20I%20am%20interested%20in%20your%20services%20please%20contact%20me%20shortly.">
+                                    <h6 style="margin: 0px; padding:14px;">SEND A WHATSAPP MESSAGE</h6>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col imgs d-flex">
+                            <img width="80%" ;
+                                src="https://webzplot.com/wp-content/uploads/2020/11/Hotel-Booking-amico-1-768x768.png.webp"
+                                alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="quote text-center">
+                <p class="fs-2">PREMIUM ECOMMERCE WEBSITES - AFFORDABLE PRICING</p>
+                </p class="fs-4">FREE DOMAIN | FREE HOSTING | FREE PROFESSIONAL EMAILS</p>
+            </div>
+            <div class="pricing">
+
+
+                <div class="row row row-cols-4">
+
+                    <div class="col mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box featured">
+                            <h3>Gold Website Package</h3>
+                            <h4><sup>₹
+                                </sup>49,999<span> </span></h4>
+                            <h4><span>E-Commerce Website</span></h4>
+
+                            <ul>
+                                <li><span class="bx bx-check">E-Commerce Website&nbsp;</span></li>
+                                <li><span class="bx bx-check">Customized Design&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Hosting 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Domain 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">SEO Ready Website&nbsp;</span></li>
+                                <li><span class="bx bx-check">Business E-Mail 10&nbsp;</span></li>
+                                <li><span class="bx bx-check">SSL Certificate&nbsp;</span></li>
+                                <li><span class="bx bx-check">Control Panel&nbsp;</span></li>
+                            </ul>
+                            <!-- <form>
+                                <script src="https://checkout.razorpay.com/v1/payment-button.js"
+                                    data-payment_button_id="pl_KS93jQtHlbAeTU" async> </script>
+                            </form> -->
+                        </div>
+                    </div>
+                    <div class="col mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box featured">
+                            <h3>Gold+ Website
+                                Package</h3>
+                            <h4><sup>₹
+                                </sup>69,999<span> </span></h4>
+                            <h4><span>Advanced E-Commerce Website</span></h4>
+
+                            <ul>
+                                <li><span class="bx bx-check">E-Commerce Website&nbsp;</span></li>
+                                <li><span class="bx bx-check">Customized Design&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Hosting 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Domain 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">SEO Ready Website&nbsp;</span></li>
+                                <li><span class="bx bx-check">Business E-Mail 10&nbsp;</span></li>
+                                <li><span class="bx bx-check">SSL Certificate&nbsp;</span></li>
+                                <li><span class="bx bx-check">Control Panel&nbsp;</span></li>
+                            </ul>
+                            <!-- <form>
+                                <script src="https://checkout.razorpay.com/v1/payment-button.js"
+                                    data-payment_button_id="pl_KS9DziOEeWv1hI" async> </script>
+                            </form> -->
+                        </div>
+                    </div>
+
+                    <div class="col mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box featured">
+                            <h3>Platinum Website Package
+                            </h3>
+                            <h4><sup>₹
+                                </sup>89,999<span> </span></h4>
+                            <h4><span>Advanced E-Commerce Website + Android App
+
+                                </span></h4>
+
+                            <ul>
+                                <li><span class="bx bx-check">E-Commerce Website</span></li>
+                                <li><span class="bx bx-check">Wallet and Referral Integration</span></li>
+                                <li><span class="bx bx-check">Abandoned Checkout</span></li>
+                                <li><span class="bx bx-check">Android Hybrid App&nbsp;</span></li>
+                                <li><span class="bx bx-check">Google Play Publishing&nbsp;</span></li>
+                                <li><span class="bx bx-check">Customized Design&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Hosting 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Domain 1 Year&nbsp;</span></li>
+                            </ul>
+                            <!-- <form>
+                                <script src="https://checkout.razorpay.com/v1/payment-button.js"
+                                    data-payment_button_id="pl_KS8bpO7Co0VNdg" async> </script>
+                            </form> -->
+                        </div>
+                    </div>
+                    <div class="col mt-4 mt-lg-0" data-aos="fade-up" data-aos-delay="200">
+                        <div class="box featured">
+                            <h3>Diamond Website Package
+                            </h3>
+                            <h4><sup>₹
+                                </sup>119,999<span> </span></h4>
+                            <h4><span>Advanced E-Commerce Website + Android App + iOS Hybrid App</span></h4>
+
+                            <ul>
+                                <li><span class="bx bx-check">E-Commerce Website</span></li>
+                                <li><span class="bx bx-check">Wallet and Referral Integration</span></li>
+                                <li><span class="bx bx-check">Abandoned Checkout</span></li>
+                                <li><span class="bx bx-check">Android Hybrid App&nbsp;</span></li>
+                                <li><span class="bx bx-check">Google Play Publishing&nbsp;</span></li>
+                                <li><span class="bx bx-check">Customized Design&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Hosting 1 Year&nbsp;</span></li>
+                                <li><span class="bx bx-check">Free Domain 1 Year&nbsp;</span></li>
+                            </ul>
+                            <!-- <form>
+                                <script src="https://checkout.razorpay.com/v1/payment-button.js"
+                                    data-payment_button_id="pl_KS8bpO7Co0VNdg" async> </script>
+                            </form> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-5 section-title text-center">
+                <h2 style="color: white;">REACH OUT TO US FOR ECOMMERCE WEBSITE DEVELOPMENT</h2>
+            </div>
+
+            <section id="contact" class="contact">
+                <div class="container" data-aos="fade-up">
+                    <div class="row">
+
+                        <div class="col-lg-5 d-flex align-items-stretch">
+                            <div class="info">
+                                <div class="address">
+                                    <i class="bi bi-geo-alt"></i>
+                                    <h4>Location:</h4>
+                                    <p>DD Puram
+                                        Bareilly<br>Uttar Pradesh 243001<br>
+                                        India <br> </p>
+                                </div>
+
+                                <div class="email">
+                                    <i class="bi bi-envelope"></i>
+                                    <h4>Email:</h4>
+                                    <p>contact@dabotics.com</p>
+                                </div>
+
+                                <div class="phone">
+                                    <i class="bi bi-phone"></i>
+                                    <h4>Call:</h4>
+                                    <p>0581-3585883, +91 8126664363</p>
+                                </div>
+                                <img src="assets/img/contact.png" alt="">
+                            </div>
+                        </div>
+
+                        <div class="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
+                            <form action="COnus.php" method="post" role="form" class="php-email-form">
+                                <div class="row">
+                                    <div class="form-group col-md-6">
+                                        <label for="name">Your Name</label>
+                                        <input type="text" name="name" class="form-control" id="name" required>
+                                    </div>
+                                    <div class="form-group col-md-6">
+                                        <label for="name">Your Email</label>
+                                        <input type="email" class="form-control" name="email" id="email" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">Subject</label>
+                                    <input type="text" class="form-control" name="subject" id="subject" required>
+                                </div>
+                                <div class="form-group">
+                                    <label for="name">Message</label>
+                                    <textarea class="form-control" name="message" rows="10" required></textarea>
+                                </div>
+                                <div class="text-center"><button type="submit">Send Message</button></div>
+                                <div class="wapp mt-4 text-center">
+                                    <a target="0"
+                                        href="https://api.whatsapp.com/send/?phone=+918126664363&text=Hi!%20I%20am%20interested%20in%20your%20services%20please%20contact%20me%20shortly.">
+                                        <h6 style="margin: 0; padding:14px;">SEND A WHATSAPP MESSAGE</h6>
+                                    </a>
+                                </div>
+                                <div class="mt-1">
+
+                                    <!-- My Google Ads Here Dont Touch This Code -->
+                                    <script async
+                                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545298674584153"
+                                        crossorigin="anonymous"></script>
+                                    <!-- FirstAd -->
+                                    <ins class="adsbygoogle" style="display:block"
+                                        data-ad-client="ca-pub-7545298674584153" data-ad-slot="4270295808"
+                                        data-ad-format="auto" data-full-width-responsive="true"></ins>
+                                    <script>
+                                    (adsbygoogle = window.adsbygoogle || []).push({});
+                                    </script>
+                                    <!-- My Google Ads Here Dont Touch This Code -->
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+            <div class="container">
+                <div class="mt-5 section-title text-center">
+                    <h2 style="color: white;">Ecommerce Website Development Service</h2>
+                </div>
+                <p class="fs-4 text-center">Dabotics India Pvt. Ltd. is an Ecommerce website development company. Get a
+                    completely
+                    custom
+                    designed and developed
+                    Ecommerce website for your brand. Represent your brand like never before by giving an amazing
+                    experience to your customers, Our Ecommerce Website Designs are exclusively designed by our
+                    professional designers to give you the best Ecommerce Website Development Service</p>
+            </div>
+
+
+        </div>
+
+    </section>
+
+    <!-- <div class="container mt-3"> -->
+
+    <!-- My Google Ads Here Dont Touch This Code -->
+    <!-- <script async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7545298674584153"
+            crossorigin="anonymous"></script> -->
+    <!-- FirstAd -->
+    <!-- <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7545298674584153"
+            data-ad-slot="4270295808" data-ad-format="auto" data-full-width-responsive="true"></ins>
+        <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+        </script> -->
+    <!-- My Google Ads Here Dont Touch This Code -->
+    <!-- </div> -->
+    <!-- End Services Sectin -->
+    <!-- ======= Footer ======= -->
+    <?php
+    include 'Footer.php'
+    ?>
+    <!-- End Footer -->
+
+</body>
+
+</html>
