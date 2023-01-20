28a7cb234d78821a5d9474f3a671d11184ab84be
IP-Coder <shrivastavaps80@gmail.com>
2023-01-20 18:03:51 +0530

    android
    android

--- Appdevelopment.php
+++ Appdevelopment.php
@@ -28,92 +28,53 @@
             <div class="row content">
                 <div class="col-lg-12">
 
-                    <p>Best Website Designing and Development Company in Bareilly
-                        Are you looking for the best website development or website designing company in Bareilly to
-                        create a professional website for your business? If yes, DABOTICS INDIA is the right choice for
-                        web designing, web development, and eCommerce website development company in Bareilly.
-                        <br>
-                        We at DABOTICS INDIA help small and large businesses to create professional, creative, and
-                        responsive websites. We develop a portfolio, corporate, eCommerce, news, and blog website with
-                        the maximum user interface and user experience.
-                        We have industries best web developers and web designers. They are experts in designing and
-                        developing all types of responsive and professional websites. Our websites take a minimum time
-                        to load and render.
-                        <br>
-                        If you want to get a professional website to grow your business online, we will help you design
-                        and promote your website under a single roof. We have proven our website designing and
-                        development skills by creating the fastest and most creative websites for various businesses. We
-                        claim to be the best web development company in Bareilly.
-                        <br>
-                        There are too many services we offer in our web designing modules. Here is the list of some most
-                        popular services we do.
-                    <h3>Responsive Web Designing</h3>
-                    Responsive web designing is commonly known as RWD. It is the art of web designing in which we
-                    create different layouts for different devices like mobile, tablets, and desktops. It adjusts
-                    its style with the width and category of the device.
-                    <h3>Re-Web Designing</h3>
-                    If you already have a website, but it doesn't meet your requirement, We will help you design it
-                    again and again. We will change the layout, color scheme, content, pages, and other things you
-                    want on your website.
-                    <h3>eCommerce Website Development</h3>
-                    In eCommerce development, we help our customers set up an eCommerce platform. We create an
-                    eCommerce platform, integrate a payment gateway, set up the delivery module, etc. We also
-                    optimize it according to search engines to be visible in top SERPs.
-                    <h3>News Website Designing</h3>
-                    If you are running a news channel or media agency, we will help you develop the best website for
-                    news and magazine. We will also assist you in getting your stories in Google's news section.
-                    Using our news website development service, you can be sure to be visible in the top search
-                    engines quickly.
-                    <h3>Portfolio Website Designing</h3>
-                    A portfolio website helps you showcase your work. We create professional and creative portfolio
-                    designs to engage more audiences easily. We also helo you write and present the right content
-                    for each module.
-                    <h3>Mobile Website Designing</h3>
-                    Mobile website designing refers to the technique of designing a mobile-only website. It helps
-                    mobile users to deliver a highly optimized user interface and user experience. It's a beneficial
-                    decision to create a mobile-first website because search engines are indexing mobile websites
-                    with priority.
-                    <h3>Real Estate Website Designing</h3>
-                    Real estate website design includes the selling and purchasing section. We offer a 100%
-                    customized real estate website to showcase the properties and deliver information about them.
-                    <h3>Blog Website Development</h3>
-                    The blog website is similar to the news website. In this, we present categories, sections, and
-                    articles. We also help bloggers to write the right and search engine-friendly content.
-                    SEO Friendly Website Development
-                    DABOTICS INDIA is the best website development company in Bareilly for SEO-friendly website
-                    development services. We optimize content, interface, loading time, and other things to increase
-                    a website's visibility in different search engines.
-                    <h3>Website Hosting</h3>
-                    Are you have a domain name and looking for the fastest web hosting to host your files? If yes,
-                    DABOTICS INDIA is the best place to get the fastest web hosting. We promise to provide a 100%
-                    uptime server to serve your website at any time.
-                    <h3>Website Maintenance</h3>
-                    Already have a website for your business and want to maintain it? If yes, DABOTICS INDIA is the
-                    right web development company in Bareilly to provide the best website maintenance services. We
-                    offer all types of maintenance services at an affordable cost.
-                    <br>
-                    <h3>What is Web Designing?</h3>
-                    Web designing is the art of creating a website and publishing it on the internet. It helps a
-                    business owner to show their products and services online. A professional and creative website
-                    increases your business visibility on the world wide web.
-                    <h3>What is the Importance of a Website?</h3>
-                    A professional and responsive website helps create an online presence and helps share quality
-                    data with your clients. Anyone can know about your products, services, and company profile by
-                    accessing your website anywhere. It also helps business owner promote their business on
-                    different digital platforms.
-                    <h3>Why Choose DABOTICS INDIA for Website Designing?</h3>
-                    <br>
-                    Want to hire a website development and website designing company in Bareilly? DABOTICS INDIA can
-                    help you create a professional and responsive website for your business. We have a team of web
-                    designers and developers with lots of industrial experience. Here are the few facts that make
-                    DABOTICS INDIA the best website development company in Bareilly. <br>
-                    1. We offer responsive web designing services at an affordable cost. <br>
-                    2. We know how to place a website in the top results of search engines. <br>
-                    3. We have an eye over all the website designing and development trends. <br>
-                    4. We optimize the website for the fastest loading maximum user interface. <br>
-                    5. Digi Friction is the fastest-growing web development company in Bareilly. <br>
-                    6. We use the latest technologies to make our services different from other companies. <br>
-                    7. We have proven our skills by developing 100+ websites for different types of business. <br>
+                    <p>IOS/Android Application Development Services India
+                        Our gathering of adaptable Mobile Application Designers is innovative and figured out how to
+                        satisfy your individual solicitations and also your business needs. With bleeding edge
+                        mechanical assemblies and advancement, our Mobile App engineers can make exceptionally flexible
+                        applications for client needs and endeavours. Our experience and past work are the shows of our
+                        quality in flexible Applications change. <br>
+                    <H3> Mobile Apps</H3>
+                    We are an Award Winning Mobile application development Company in Bareilly. Similar to a Pioneer
+                    in versatile application advancement we are required to draw out another portable application
+                    plan and design again and again. The more perplexing the assignment is the better is released
+                    out of our versatile application designers. The backend is a no lesser activity than making an
+                    android or IOS portable application. We have the ability to convey the best versatile
+                    application revealing motor and portable application backend framework. We utilize a combination
+                    of intuitive and innovative technologies to design and develop mobile applications that are more
+                    perfect and easy to understand Utilizing the cutting-edge innovations accessible in the market
+                    we draw out the best in a Mobile Application Development that could surpass your desires
+                    effortlessly. You share your thought and we transform it into a group satisfying Mobile
+                    application. Our solutions are top notch and state of the art and all this at appealing prices.
+                    Some of the features we can add to your idea: <br>
+                    • a. Cloud Storage Compatibility <br>
+                    • b. Cross Platform Compatibility <br>
+                    <h3>Android App</h3>
+                    We are the full-fledged service provider of Android Apps based in Bareilly. We have the most
+                    experienced Android engineers with the in-house foundation for outline, improvement, testing and
+                    arrangement of the application. We utilize the blend of UI/UX to convey eye-getting
+                    applications. Our applications are in reverse good and ready to convey its ideal execution with
+                    less information utilization and no costly equipment required. The innovation utilized in the
+                    application is in order to guarantee a strong spine, diminished multifaceted nature in
+                    engineering and less demanding to introduce on the majority of the Android device accessible. <br>
+                    <h3>IOS app development</h3>
+                    Application Development Company is a reputed IOS App Development company that is headquartered
+                    in Bareilly. We have an improving background of conveying IOS App Development administrations
+                    from past and our customer base is spread over the world. Profiting by a similar we meet client
+                    requests with best ROI and quality and that too at focused costs.
+                    Our in-house group of architects and engineers work in close coordination with the end clients
+                    and customer in order to envision the outline of the application and at last make an application
+                    with a successful, natural and eye-getting plan. <br>
+                    Before the application is formally propelled we make it a point to guarantee that all
+                    pre-organization tests are hurried to guarantee that the application is free from any
+                    specialized mistake or bug. As every one of these procedures is executed in-house as we an
+                    outcome we spare a great deal of urgent time and cash for both the customer and the end client. <br>
+                    <h3>Hybrid App Development</h3>
+                    Hybrid Apps are made utilizing HTML5, CSS3 & JavaScript. They provide more access to device
+                    features and capabilities. They are developed using open source technologies providing a cost
+                    effective solution. They have highly interactive and impressive user interfaces that deliver an
+                    improved performance. They can be downloaded and used directly and no more need to browse them.
+                    They are less resource intensive and have enhanced security features.
                     </p>
 
                 </div>
